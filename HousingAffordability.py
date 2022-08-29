3c07ebe2a5a2137a0c89bfdc87fbac052432f313
Michael K. Roberson <65736600+mkroberson0208@users.noreply.github.com>
2022-08-29 07:56:46 -0400

    Update HousingAffordability.py

--- HousingAffordability.py
+++ HousingAffordability.py
@@ -11,7 +11,8 @@ import requests
 import matplotlib.pyplot as plt
 
 plt.rcParams["font.family"] = "arial"
-plt.style.use('seaborn')
+plt.style.use('seaborn-whitegrid')
+plt.rcParams["legend.frameon"] = True
 
 def read_multiple(l: list):
     my_api = '499659ea0d975475c7302585ae1c6d7a'
@@ -28,7 +29,7 @@ def read_multiple(l: list):
         tmp = tmp.merge(df[['date',series_id]],on='date',how='outer')
     return tmp
 
-test = ['CSUSHPISA','MORTGAGE30US','MEHOINUSA646N']
+test = ['MSPUS','MORTGAGE30US','MEHOINUSA646N']
 df = read_multiple(test)
 df.index = pd.to_datetime(df['date'])
 df = df.sort_index()
@@ -41,4 +42,15 @@ for c in df.columns[1:]:
     fig,ax=plt.subplots(1,1)
     ax.plot(df_plot.index,df_plot)
     plt.savefig(str(c)+'_plot.png')
+    
+price = 0.8*df['MSPUS']
+rate = df['MORTGAGE30US']/100
+i = rate/12
+term = 30
+n = term*12
+
+df['monthly_pmt'] = (price*(((i)*(1+i)**n)/(((1+i)**n)-(1))))
+df['annual_pmt'] = df['monthly_pmt']*12
+df['all_in_cost'] = (price*(((i)*(1+i)**n)/(((1+i)**n)-(1))))*n + 0.2*df['MSPUS']
+df['pct_housing_costs'] = df['annual_pmt']/df['MEFAINUSA646N']
 
