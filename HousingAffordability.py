0b5eefaa338cc344d40f14b3c650171883439914
Michael K. Roberson <65736600+mkroberson0208@users.noreply.github.com>
2022-09-27 13:36:51 -0400

    Update HousingAffordability.py

--- HousingAffordability.py
+++ HousingAffordability.py
@@ -1,18 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 14 09:12:11 2021
 
-@author: Michael
-"""
 import os
 import numpy as np
 import pandas as pd
 import requests
 import matplotlib.pyplot as plt
-
-plt.rcParams["font.family"] = "arial"
-plt.style.use('seaborn-whitegrid')
-plt.rcParams["legend.frameon"] = True
+from matplotlib import ticker
 
 def read_multiple(l: list):
     my_api = '499659ea0d975475c7302585ae1c6d7a'
@@ -35,31 +27,35 @@ df.index = pd.to_datetime(df['date'])
 df = df.sort_index()
 df = df.fillna(method='ffill')
 
+plt.rcParams["font.family"] = "arial"
+plt.style.use('seaborn-whitegrid')
+
 os.chdir('C:\\Users\\Michael\\Desktop')
-for c in df.columns[1:]:
-    print(c)
-    df_plot = df[c].dropna()
-    fig,ax=plt.subplots(1,1)
-    ax.plot(df_plot.index,df_plot)
-    plt.savefig(str(c)+'_plot.png')
-    
+fig,axes=plt.subplots(2,2)
+i = 0 
+while i < 3:
+    ax = axes.flat[i]
+    ax.plot(df.iloc[:,i+1])
+    ax.set_title(df.columns[i+1])
+    i += 1
+ax = axes.flat[i]
+ax.axis('off')
+plt.tight_layout()
+plt.savefig('test_plot.png')
+
 price = 0.8*df['MSPUS']
 rate = df['MORTGAGE30US']/100
 i = rate/12
 term = 30
 n = term*12
-
 df['monthly_pmt'] = (price*(((i)*(1+i)**n)/(((1+i)**n)-(1))))
 df['annual_pmt'] = df['monthly_pmt']*12
 df['all_in_cost'] = (price*(((i)*(1+i)**n)/(((1+i)**n)-(1))))*n + 0.2*df['MSPUS']
-df['pct_housing_costs'] = df['annual_pmt']/df['MEFAINUSA646N']
-
-df_plot = df.dropna()
+df['pct_housing_costs'] = df['annual_pmt']/df['MEHOINUSA646N']
 
 fig,ax = plt.subplots(1,1,dpi=500)
 fig.set_size_inches(6, 4)
 fig.suptitle('Mortgage Payment as % Income')
-ax.plot(df_plot.DATE,df_plot.pct_housing_costs,c='navy')
+ax.plot(df.pct_housing_costs,c='navy')
 ax.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1,decimals=0))
 plt.savefig('test.png')
-
